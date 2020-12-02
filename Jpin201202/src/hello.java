be6dd9d41296268ecadbdc4425cb0ac7ce852c16
github IwaiSumire <opz09966@gmail.com>
2020-12-03 00:21:50 +0900

    JPIN受講

---
+++ Jpin201202/src/hello.java
@@ -0,0 +1,15 @@
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("岩井菫");
+		System.out.println(1111);
+
+		int a = 10;
+
+
+		int c = a++;
+
+		System.out.println(a);
+
+		System.out.println(c);
+	}
+}
