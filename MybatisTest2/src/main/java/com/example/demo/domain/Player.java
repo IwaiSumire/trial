b561cfd8cada6f5cf242a49e5332d8e63bb847da
github IwaiSumire <opz09966@gmail.com>
2020-12-13 17:51:21 +0900

    MyBatis

---
+++ MybatisTest2/src/main/java/com/example/demo/domain/Player.java
@@ -0,0 +1,14 @@
+package com.example.demo.domain;
+
+
+import lombok.Data;
+
+@Data
+public class Player {
+
+	private Long id;
+	private String name;
+	private String team;
+	private String position;
+
+}
