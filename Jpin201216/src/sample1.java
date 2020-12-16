5014f25b5be65b9fc25626dfc631a1e082e2d0a5
github IwaiSumire <opz09966@gmail.com>
2020-12-16 21:48:52 +0900

    Jpin

---
+++ Jpin201216/src/sample1.java
@@ -0,0 +1,15 @@
+
+public class sample1 {
+
+	public static void main(String[] args) {
+
+		String a = "abc" + 'd' + 1 + 2;
+
+		System.out.println(a);
+
+		int num = Integer.parseInt("100");
+
+		System.out.println(num);
+	}
+
+}
