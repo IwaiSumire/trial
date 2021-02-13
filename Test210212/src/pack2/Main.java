b6ead7b6e4a21bf7cb0fa261bcb17c15a88d0bce
github IwaiSumire <opz09966@gmail.com>
2021-02-13 09:23:50 +0900

    test

--- Test210212/src/pack2/Main.java
+++ Test210212/src/pack2/Main.java
@@ -8,29 +8,29 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		int a = 0;
+		int count = 0;
 
 		int n = sc.nextInt();
 
 		if (n % 100 > 1) {
 
-			a = -1;
+			count = -1;
 
 		} else if (n >= 500) {
 
-			a = n / 500;
+			count = n / 500;
 
-			if (n - (500 * a) >= 100) {
+			if (n - (500 * count) >= 100) {
 
-				int s = n - a * 500;
+				int s = n - count * 500;
 
-				a = a + (s / 100);
+				count = count + (s / 100);
 
 			}
 
 		}
 
-		System.out.println(a);
+		System.out.println(count);
 
 		sc.close();
 	}
